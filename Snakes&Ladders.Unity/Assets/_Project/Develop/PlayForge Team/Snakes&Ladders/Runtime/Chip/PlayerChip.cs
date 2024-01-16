52ff0ef9773705f335e57649cf762c7734c32145
Sergei Lind <lind-2004@mail.ru>
2024-01-16 18:15:10 +0300

    Add chips

---
+++ Snakes&Ladders.Unity/Assets/_Project/Develop/PlayForge Team/Snakes&Ladders/Runtime/Chip/PlayerChip.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.SnakesAndLadders.Runtime.Runtime.Chip
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public sealed class PlayerChip : MonoBehaviour
+    {
+        public void SetSprite(Sprite sprite)
+        {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = sprite;
+        }
+    }
+}
