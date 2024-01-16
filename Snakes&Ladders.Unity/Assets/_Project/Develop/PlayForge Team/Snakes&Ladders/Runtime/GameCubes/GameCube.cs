06ccfe8820983c317291750519c9635234cad460
Sergei Lind <lind-2004@mail.ru>
2024-01-16 20:11:35 +0300

    Add GameCube.cs

---
+++ Snakes&Ladders.Unity/Assets/_Project/Develop/PlayForge Team/Snakes&Ladders/Runtime/GameCubes/GameCube.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.SnakesAndLadders.Runtime.Runtime.GameCubes
+{
+    public sealed class GameCube : MonoBehaviour
+    {
+        [SerializeField] private Vector3[] cubeSidesEulers;
+        
+        public void ShowCube()
+        {
+            SetActiveCube(true);
+        }
+
+        public void HideCube()
+        {
+            SetActiveCube(false);
+        }
+
+        private void SetActiveCube(bool value)
+        {
+            gameObject.SetActive(value);
+        }
+        
+        public int ThrowCube()
+        {
+            ShowCube();
+            var randomCubeValue = Random.Range(0, cubeSidesEulers.Length);
+            RotateCube(cubeSidesEulers[randomCubeValue]);
+            return randomCubeValue + 1;
+        }
+
+        private void RotateCube(Vector3 cubeEuler)
+        {
+            transform.eulerAngles = cubeEuler;
+        }
+    }
+}
