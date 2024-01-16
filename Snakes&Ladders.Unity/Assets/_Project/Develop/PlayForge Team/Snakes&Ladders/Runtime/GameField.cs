4588d2506cce90f3448465daf35f66cd30b85b0b
Sergei Lind <lind-2004@mail.ru>
2024-01-16 21:26:10 +0300

    Add GameField.cs

---
+++ Snakes&Ladders.Unity/Assets/_Project/Develop/PlayForge Team/Snakes&Ladders/Runtime/GameField.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.SnakesAndLadders.Runtime.Runtime
+{
+    public sealed class GameField : MonoBehaviour
+    {
+        [SerializeField] private Transform firstCellPoint;
+        [SerializeField] private Vector2 cellSize;
+        [SerializeField] private int cellsCount = 100;
+        [SerializeField] private int cellsInRow = 10;
+        private Vector2[] _cellsPositions;
+        
+        public void FillCellsPositions()
+        {
+            _cellsPositions = new Vector2[cellsCount];
+            var right = true;
+            _cellsPositions[0] = firstCellPoint.position;
+
+            for (var i = 1; i < _cellsPositions.Length; i++)
+            {
+                var needUp = i % cellsInRow == 0;
+                if (needUp)
+                {
+                    right = !right;
+                    _cellsPositions[i] = _cellsPositions[i - 1] + Vector2.up * cellSize.y;
+                }
+                else
+                {
+                    var xSign = right ? 1f : -1f;
+                    var deltaX = xSign * cellSize.x;
+                    _cellsPositions[i] = _cellsPositions[i - 1] + Vector2.right * deltaX;
+                }
+            }
+        }
+        
+        public Vector2 GetCellPosition(int id)
+        {
+            if (id < 0 || id >= _cellsPositions.Length)
+            {
+                return Vector2.zero;
+            }
+            return _cellsPositions[id];
+        }
+    }
+}
